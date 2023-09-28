86233dc1c3c017844ee5b3c5f7e9123d44e44660
Larissa Sevilha <sevilha.larissa10@gmail.com>
2023-09-28 14:30:24 -0400

    configurando projeto

---
+++ agenda-de-contatos/db/migrate/20230928182256_create_agendas.rb
@@ -0,0 +1,10 @@
+class CreateAgendas < ActiveRecord::Migration[7.0]
+  def change
+    create_table :agendas do |t|
+      t.string :title
+      t.integer :number
+
+      t.timestamps
+    end
+  end
+end
